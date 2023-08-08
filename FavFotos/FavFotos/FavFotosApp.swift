095e79952043195549105760b03fd65da8bdd485
Dean Thompson <deanthompson.music@gmail.com>
2023-08-08 21:50:56 +0900

    Initial Commit

---
+++ FavFotos/FavFotos/FavFotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFotosApp.swift
+//  FavFotos
+//
+//  Created by Dean Thompson on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct FavFotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
