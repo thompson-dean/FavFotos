3dff14252ff79edbb6763fe3e856fd114e1ffdfa
Dean Thompson <deanthompson.music@gmail.com>
2023-08-14 23:43:46 +0900

    remove apiKey

--- FavFotos/FavFotos/Utilities/Constants.swift
+++ FavFotos/FavFotos/Utilities/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Constants {
     static let curatedAPI: String = "https://api.pexels.com/v1/curated"
     static let searchAPI: String = "https://api.pexels.com/v1/search?query="
-    static let apiKey: String = "jFH9f2Csqg0G65N55Cf5F03qDvUL725Ydsc3CNDYD1zqZylHfPAuBeHk"
+    static let apiKey: String = ""
     static let perPage: Int = 15
     static let containerName: String = "FavoritesContainer"
     static let entityName: String = "PhotoEntity"
